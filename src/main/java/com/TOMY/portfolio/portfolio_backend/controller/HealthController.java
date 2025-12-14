3de64b70708887927051048f63b1567c91d7c1f1
Tomas <96267389+T0MY1206@users.noreply.github.com>
2025-12-14 08:16:13 -0300

    Ajuste controller health

--- src/main/java/com/TOMY/portfolio/portfolio_backend/controller/HealthController.java
+++ src/main/java/com/TOMY/portfolio/portfolio_backend/controller/HealthController.java
@@ -4,14 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class PruebaController {
-    @RestController
-    @RequestMapping("/api/health")
-    public class HealthController {
-    
-        @GetMapping
-        public String health() {
-            return "OK";
-        }
-    } 
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "OK";
+    }
 }
